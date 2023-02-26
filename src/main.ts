e8a5d577ae76318dcb00130543dc7a9b165da1cb
Zimbru76 <105271130+Zimbru76@users.noreply.github.com>
2023-02-26 10:08:19 +0200

    Add ngOnInit

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { catchError, map, of } from 'rxjs';
 
 @Component({
   selector: 'my-app',
@@ -16,6 +17,23 @@ import { bootstrapApplication } from '@angular/platform-browser';
 })
 export class App {
   name = 'Angular';
+
+  ngOnInit() {
+    of(2, 4, 6)
+      .pipe(
+        map((i) => {
+          if (i === 6) {
+            throw 'Error!';
+          }
+          return i;
+        }),
+        catchError((err) => of('Reached six'))
+      )
+      .subscribe({
+        next: (x) => console.log(x),
+        error: (err) => console.log(err),
+      });
+  }
 }
 
 bootstrapApplication(App);
